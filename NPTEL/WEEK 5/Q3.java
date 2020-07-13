356b965b4c23d31958d6a05a453e6715a1257405
im876 <64376463+im876@users.noreply.github.com>
2020-07-13 15:15:13 +0530

    Add files via upload

---
+++ NPTEL/WEEK 5/Q3.java
@@ -0,0 +1,28 @@
+package NPTEL_WEEK05;
+import java.util.Scanner;
+
+public class Q3 
+{
+	public static void main(String[] args) 
+	{ 
+		int a, b;
+		Scanner input = new Scanner(System.in);
+
+		int result;  
+		a = input.nextInt();
+		b = input.nextInt();
+
+    // try block to divide two numbers and display the result
+		try 
+		{
+            result = a/b;
+            System.out.println(result);
+   	     }
+        // catch block to catch the ArithmeticException
+        catch (ArithmeticException e) 
+		{
+           System.out.println("Exception caught: Division by zero.");
+        }
+
+	}
+}
---
+++ NPTEL/WEEK 5/Q5.java
@@ -0,0 +1,32 @@
+package NPTEL_WEEK05;
+import java.util.Scanner;
+public class Q5
+{
+	public static void main (String   args[ ] ) 
+	{
+           Scanner scan = new Scanner(System.in);
+            	int i = scan.nextInt();
+            	int j;  
+            	try 
+            	{
+            		switch (i) 
+            		{
+            		case 0 : 
+            			int zero = 0; 
+            			j = 92/ zero; 		
+            			break;
+            		case 1 : 
+            				int b[ ] = null; 
+            				j = b[0] ; 	
+            				break;
+            		default:
+            				System.out.println("No exception");
+            		} 		
+            	}
+            // catch block			
+            	catch (Exception e) 
+            	{		
+            		System.out.println(e) ;
+            	}
+	}
+}
