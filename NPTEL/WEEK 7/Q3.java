334f7f95f2907b7faba5d8c342af4f09c558dcb7
im876 <64376463+im876@users.noreply.github.com>
2020-07-13 15:17:04 +0530

    Add files via upload

---
+++ NPTEL/WEEK 7/Q3.java
@@ -0,0 +1,21 @@
+package NPTEL_WEEK07;
+import java.util.*;
+public class Q3 
+{  
+    public static void main(String[] args) 
+    { 
+       try
+       {	
+    	   byte barr[]={'N','P','T','E','L','-','J','A','V','A','J','A','N','-','N','O','C','C','S','E'};
+    	   Scanner inr = new Scanner(System.in);
+    	   int n = inr.nextInt();
+    	   String s2 = new String(barr,n,1);  
+    	   System.out.println(barr[n]);
+    	   System.out.println(s2); 
+       }
+       catch (Exception e)
+       {
+    	   System.out.println("exception occur");
+	   }	   
+    }  
+}  
