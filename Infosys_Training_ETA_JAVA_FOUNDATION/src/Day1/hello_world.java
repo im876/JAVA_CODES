0c049a9e92f03d3f6ed8cc904fb9c5638b328f1f
Ishan Modi <64376463+im876@users.noreply.github.com>
2022-08-16 11:41:15 +0530

    Add files via upload

---
+++ Infosys_Training_ETA_JAVA_FOUNDATION/src/Day1/hello_world.java
@@ -0,0 +1,18 @@
+package Day1;
+
+public class hello_world {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.print("Welcome to Java Programming \n");
+		System.out.print("Good Morning\n");
+		
+		// Else
+		System.out.println("Hellllllu");
+		
+		
+		//Shortcut for typing println : sysout, then ctrs+space
+		System.out.println("shortcut try");
+	}
+
+}
