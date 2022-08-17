fd4674135e62a1f925a25801abb87c09752d8428
Ishan Modi <64376463+im876@users.noreply.github.com>
2022-08-17 09:37:31 +0530

    Add files via upload

---
+++ Infosys_Training_ETA_JAVA_FOUNDATION/src/Day1/variables_demo.java
@@ -0,0 +1,19 @@
+package Day1;
+
+public class variables_demo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a1 = 100;
+		int a2 = 156, a3 = 23;
+		
+		int sum = a1+a2;
+		System.out.println("sum = " + sum);
+		System.out.println(a1+a2);
+		System.out.println("sum = " + (a1+a2+a3));
+		
+		int a4=10,a5=10,a6=10;
+		System.out.println(a4+a5+a6);
+	}
+
+}
