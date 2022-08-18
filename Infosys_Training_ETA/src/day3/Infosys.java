923a27c3b9636bb5645b2ff2b6980bc44eb07439
Ishan Modi <64376463+im876@users.noreply.github.com>
2022-08-18 11:39:34 +0530

    Create Infosys.java

---
+++ Infosys_Training_ETA/src/day3/Infosys.java
@@ -0,0 +1,76 @@
+package day3;
+import java.util.Scanner;  
+public class Infosys {
+
+	public static void main(String[] args) {
+		
+		Scanner sc= new Scanner(System.in);
+		// TODO Auto-generated method stub
+		Employee IM = new Employee();
+		IM.EmpID = 101;
+		IM.Name = "Ishan Modi";
+		IM.Address = "Pune";
+		IM.dob = "20.05.01";
+		IM.blood_grp = "A+";
+		
+		Employee IS = new Employee();
+		IS.EmpID = 102;
+		IS.Name = "Ishan Srivastava";
+		IS.Address = "Pune";
+		IS.dob = "06.01.00";
+		IS.blood_grp = "AB+";
+		
+		Employee IP = new Employee();
+		IP.EmpID = 103;
+		IP.Name = "Isha Pathak";
+		IP.Address = "Pune";
+		IP.dob = "23.02.01";
+		IP.blood_grp = "B+";
+		
+		System.out.print("Enter Employee Number - ");  
+		int search= sc.nextInt();  
+		
+		switch(search)
+		{
+		case 101:
+			System.out.println("Emp ID : "+IM.EmpID);
+			System.out.println("Name : "+IM.Name);
+			System.out.println("Address : "+IM.Address);
+			System.out.println("DOB : "+IM.dob);
+			System.out.println("Blood Group : "+IM.blood_grp);
+			System.out.println(" ");
+			
+			System.out.println(IM);
+			break;
+		case 102:
+			System.out.println("Emp ID : "+IS.EmpID);
+			System.out.println("Name : "+IS.Name);
+			System.out.println("Address : "+IS.Address);
+			System.out.println("DOB : "+IS.dob);
+			System.out.println("Blood Group : "+IS.blood_grp);
+			System.out.println(" ");
+			break;
+		case 103:
+			System.out.println("Emp ID : "+IP.EmpID);
+			System.out.println("Name : "+IP.Name);
+			System.out.println("Address : "+IP.Address);
+			System.out.println("DOB : "+IP.dob);
+			System.out.println("Blood Group : "+IP.blood_grp);
+			System.out.println(" ");
+			break;
+		default :
+			System.out.println("Enter Valid Data");
+		}
+	}
+
+}
+
+
+class Employee
+{
+	int EmpID;
+	String Name;
+	String Address;
+	String dob;
+	String blood_grp;
+}
