b3014a7ab1de6f8d924cbc81507171fea0848d0a
Ishan Modi <64376463+im876@users.noreply.github.com>
2022-08-22 22:03:05 +0530

    Create a_024_lucky_number.java

---
+++ Infosys_Training_ETA/src/Lex_Introduction_To_Java/a_024_lucky_number.java
@@ -0,0 +1,36 @@
+package Lex_Introduction_To_Java;
+
+public class a_024_lucky_number 
+{
+
+	public static void main(String[] args) 
+	{
+		// TODO Auto-generated method stub
+		int a = 1623;
+		int length = (int)((Integer.toString(a)).length());
+		//System.out.println(2%2);
+		int temp = a;
+		int sum = 0;
+		for(int i = 1; i<=length; i++)
+		{
+			int r = a%10;
+			System.out.println("r " +r);
+			if(i%2==0)
+			{
+				sum = sum + (r*r);
+				System.out.println("Sum "+sum);
+			}
+			a = a/10;
+			
+		}
+		if(temp%sum == 0)
+		{
+			System.out.println("Lucky");
+		}
+		else
+		{
+			System.out.println("Not Lucky");
+		}
+	}
+
+}
