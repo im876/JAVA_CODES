faf7b9a4353d61af6df728192e1f0baed2eb3aa5
Ishan Modi <64376463+im876@users.noreply.github.com>
2022-08-23 11:44:10 +0530

    Create a_026_calculator.java

---
+++ Infosys_Training_ETA/src/Lex_Introduction_To_Java/a_026_calculator.java
@@ -0,0 +1,21 @@
+package Lex_Introduction_To_Java;
+
+public class a_026_calculator {
+
+	public static void main(String args[]) {
+		Tester calculator = new Tester();
+		// Invoke the method findAverage of the Calculator class and display the average
+		calculator.findAverage(2,3,4);
+	}
+
+}
+
+class Tester {
+
+	// Implement your code here
+		public void findAverage(int number1, int number2, int number3)
+		{
+			System.out.println("bleh");
+		}
+	
+}
